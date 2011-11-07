1aeb8cea151a2a92bb696a3cc38ffaa98fe830af
Edwin Andrianto <admin@edwinners.co.cc>
2011-11-07 14:51:37 +0700

    menambah fungsi cek dan proses login

--- application/models/mlogin.php
+++ application/models/mlogin.php
@@ -5,8 +5,23 @@ class Mlogin extends CI_Model{
         parent::__construct();
     }
 
-    function index(){
+    function cek($username, $password){
+        $this->db->where('username',$username);
+        $this->db->where('password', md5($password));
+        $result=$this->db->get('user');
+        if($result->num_rows()==1){
+            return true;
+        }
         
+    }
+
+    function proses($username, $password){
+        $this->db->where('username', $username);
+        $this->db->where('password', md5($password));
+        $result=$this->db->get('user');
+        if($result->num_rows()==1){
+            return $result->row_array();
+        }
     }
 
 }
