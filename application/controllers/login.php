013f66b4d7292300ea5dea1d0e6ec38411dfd922
Edwin Andrianto <admin@edwinners.co.cc>
2011-11-07 15:11:36 +0700

    menambah fungsi otentifikasi dan logout

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,15 +1,46 @@
 <?php
 
-class Login extends CI_Controller{
+class Login extends CI_Controller {
 
-    function  __construct() {
+    function __construct() {
         parent::__construct();
+        $this->load->model('mlogin');
     }
 
-    function index(){
+    function index() {
+        if ($this->session->userdata('login') == true) {
+            redirect('admin');
+        }
         $this->load->view('login');
     }
 
-}
+    function otentik() {
+        $this->form_validation->set_rules("username", "username", "required");
+        $this->form_validation->set_rules("password", "password", "required");
+        if ($this->form_validation->run() == true) {
+            $username = $this->input->post("username", true);
+            $password = $this->input->post("password", true);
+            if ($this->mlogin->cek($username, $password) == true) {
+                $session = $this->mlogin->proses($username, $password);
+                $this->session->set_userdata("id", $session['id']);
+                $this->session->set_userdata("login", "true");
+                redirect("admin", "refresh");
+            } else {
+                $this->session->set_flashdata("notif", "Tidak ada dalam system");
+                redirect("login", "refresh");
+            }
+        } else {
+            $this->index();
+        }
+    }
 
+    function logout() {
+        $this->session->unset_userdata("id");
+        $this->session->unset_userdata("login");
+        $this->session->sess_destroy();
+        $this->session->set_flashdata("notif", "Sudah keluar dari sistem");
+        redirect('login');
+    }
+
+}
 ?>
