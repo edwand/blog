90dadc87d7328ec756b7b6fdc47a485427c76dfc
Edwin Andrianto <admin@edwinners.co.cc>
2011-11-07 14:00:44 +0700

    menambah konstruktor dan index

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,7 +1,15 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+class Login extends CI_Controller{
+
+    function  __construct() {
+        parent::__construct();
+    }
+
+    function index(){
+        
+    }
+
+}
 
 ?>
